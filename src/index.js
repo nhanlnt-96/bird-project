182bd22d67c3b0eca0701dc148ba70f0946efa09
nhanlnt-96 <nhanlnt@hotmail.com>
2022-05-08 00:03:40 +0700

    remove irrelated code

--- src/index.js
+++ src/index.js
@@ -4,10 +4,6 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import store from "./redux/store";
 import {Provider} from "react-redux";
-import AOS from "aos";
-import "aos/dist/aos.css";
-
-AOS.init();
 
 ReactDOM.render(
   <Provider store={store}>
