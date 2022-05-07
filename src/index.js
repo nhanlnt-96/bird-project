327ba8a6a2274014d6cef7f22fa17e9f53d17340
nhanlnt-96 <nhanlnt@hotmail.com>
2022-05-07 18:52:39 +0700

    dev-v2: initial new version

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import store from "./redux/store";
 import {Provider} from "react-redux";
-import "./styles/reset.css";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
